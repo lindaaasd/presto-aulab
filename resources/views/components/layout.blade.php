b179080e586589a175a7aa7a157b2394b621ba1c
Linda Giurca <lindaaa@Lindas-MacBook-Air.local>
2022-02-07 15:30:54 +0100

    iniziata la navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,14 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+     {{-- GOOGLE FONTS  --}}
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    {{-- ** FONT AWESOME ** --}}
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Presto</title>
+
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+
 </head>
 <body>
 
 
     <x-navbar/>
-    {{$slot}}
+
+    <section>
+        <div class="vh-100">
+
+            {{$slot}}
+        </div>
+    </section>
+  
+
+
     <x-footer/>
 
     
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg mx-5">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="./"> 
+       <img class="" width="80" height="80" src="./media/logo.png" alt="logo"> 
+        </a>
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Categorie
+        </a>
+        <form class="d-flex ms-5">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <li><a class="dropdown-item" href="#">Categoria</a></li>
+        <li><a class="dropdown-item" href="#">Un'altra categoria</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="#">Categoria</a></li>
+      </ul>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#"> <i class="fas fa-user"></i> </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>

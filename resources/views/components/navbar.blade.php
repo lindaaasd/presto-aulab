681d4cf2ad20f776eb555d204538a6f85e449170
Luca <Luca De Amicis>
2022-02-09 17:36:01 +0100

    sistemato logout nella navbar e aggiunto dropdown menu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,7 +2,7 @@
     <nav class="container-fluid nav-box">
         <div class="row align-items-center">
             <div class="nav-logo col-4 col-md-2 m-0 p-0 d-flex justify-content-center">
-                <a class="navbar-brand" href="{{route('welcome')}}">
+                <a class="navbar-brand" href="{{ route('welcome') }}">
                     <img class="" width="80" height="80"
                         src="https://assets.website-files.com/5ec440af4659932990a1020c/6126238e144f7970c00d7c57_60f17a27f09c3f9cecf7ef53_footer-avatar.png"
                         alt="logo">
@@ -15,30 +15,57 @@
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                     @foreach ($categories as $category)
-                        <li><a class="dropdown-item" href="{{route('announcement.category', compact('category'))}}" id="{{ $category->id }}">{{ $category->name }}</a></li>
+                        <li><a class="dropdown-item" href="{{ route('announcement.category', compact('category')) }}"
+                                id="{{ $category->id }}">{{ $category->name }}</a></li>
                     @endforeach
                     {{-- <li><a href="{{route('announcement.category', compact('category','announcements'))}}"></a></li> --}}
                 </ul>
             </div>
-            <div class="nav-search col-12 col-md-6 justify-content-start">
+            <div class="nav-search col-12 col-md-6 justify-content-between">
                 <form class="d-flex ms-5 w-50">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn search-btn" type="submit">Search</button>
                 </form>
-            </div>
-            @guest
-                <div class="nav-login col-4 col-md-2 d-flex">
-                    <a class="a-nav nav-link" href="{{ route('login') }}"> Accedi <i class="fas fa-user"></i> </a>
-                    @else
-                    <div class="nav-item ">{{ Auth::user()->name }}</div>
-                    <div class="nav-item "><a class="fw-bold" href=" {{ route('logout') }}"
-                                onclick="event.preventDefault(); document.querySelector('#logout-form').submit();">
-                                Logout</a></div>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                @endguest
+
+
+                <div>
+                    @guest
+                        <div class="nav-login col-4 col-md-2 d-flex ms-5">
+                            <a class="a-nav nav-link" href="{{ route('login') }}"> Accedi <i class="fas fa-user"></i>
+                            </a>
+                        @else
+                            <li class="nav-item dropdown me-4">
+                                <a class="nav-link dropdown-toggle user-name" href="#" id="navbarDropdown" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    {{ Auth::user()->name }}
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li class="ps-3"><a class="dropdown-item " href="#">
+                                            <div class="nav-item user-name"><a class="fw-bold"
+                                                    href=" {{ route('logout') }}"
+                                                    onclick="event.preventDefault(); document.querySelector('#logout-form').submit();">
+                                                    Logout</a></div>
+                                        </a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                            </form>
+
+                        </div>
+                    @endguest
+                </div>
+
+
             </div>
         </div>
     </nav>
 </section>
+
+
+

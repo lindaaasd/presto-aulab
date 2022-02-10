c07f71eb2626b2ef678c913886f76d970c5fb224
Linda Giurca <lindaaa@Lindas-MacBook-Air.local>
2022-02-10 10:19:34 +0100

    creato il revisor middleware & controller

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -12,7 +12,7 @@ class AnnouncementController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth', 'auth.revisor']);
     }
 
     public function categoryAnnouncement($id)

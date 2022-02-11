3beb3b4d9b216c58ada25f127342ebcf3af9eeab
Linda Giurca <lindaaa@Lindas-MacBook-Air.local>
2022-02-11 08:56:14 +0100

    slight change at the login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,9 @@
                       </label>
                       <div class="d-flex justify-content-center flex-column align-items-center">
                         <button class="login-submit text-center category-btn" type="submit">Sign In</button>
-                        <p class="forgot-pass">Forgot Password ?</p>
+                        <a href="{{route('register')}}">
+                        <p class="forgot-pass">New user? Register here</p>
+                        </a>
 
                       </div>
           
